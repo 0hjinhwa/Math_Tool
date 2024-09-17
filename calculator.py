a57b50a34bef68221823ec29ea2172b261e84f02
jinhwa <fivejinhwa@naver.com>
2024-09-17 15:31:04 +0900

    change errors
    editing master branch, modified

--- calculator.py
+++ calculator.py
@@ -5,16 +5,10 @@ return a+b
 def subtract(a,b):
 return a-b
 
-<<<<<<< HEAD
 def multify(a,b):
 return a*b
 
 def divide(a,b):
 return a/b
 
-# 기본 계산기
-=======
-def multiply(a,b):
-	return a*b
 
->>>>>>> 2ed9865f9dfa21354a70fe2bb05f306dba256e06
